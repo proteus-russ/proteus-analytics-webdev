f33df687890d85b93daabfd89ed6dd4c1bd1e1aa
Russ Tennant <russ@proteus.co>
2018-10-18 10:08:49 -0500

    Factor out variable in example

--- web/src/javascript/default/pages/example-init.js
+++ web/src/javascript/default/pages/example-init.js
@@ -6,7 +6,8 @@ pa("provide", "cleanUrlTracker", CleanUrlTracker);
 pa("require", "eventTracker", {
 	"attributePrefix": "data-pa-"
 });
-pa("set", {'engage_account_id':'$accountid','temporality':'$temporality','member_type':'$memberType'});
+const dims = {'engage_account_id': '$accountid', 'temporality': '$temporality', 'member_type': '$memberType'};
+pa("set", dims);
 pa("require", "outboundLinkTracker");
 pa("require", "impressionTracker", {attributePrefix: "data-pa-", "elements": ["questions-greeting"]});
 pa("require", "cleanUrlTracker", {stripQuery: true, trailingSlash: "remove"});
